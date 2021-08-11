026db31614bf741aae6642704654ed53fc708902
Kevin McGrevey <niv3kmcg@gmail.com>
2021-08-10 18:26:36 -0600

    Adds Round class

---
+++ lib/round.rb
@@ -0,0 +1,45 @@
+class Round
+  attr_accessor :deck, :turns
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+
+  def current_card
+    @deck.cards.first
+  end
+
+  def take_turn(guess)
+    turn = Turn.new("Juneau", current_card)
+    turns << turn
+    @deck.cards.shift
+    
+    turn
+  end
+
+  def number_correct
+    correct.length
+  end
+  
+  def number_correct_by_category(category)
+    correct.find_all { |turn| turn.card.category == category }.length
+  end
+
+  def correct
+    @turns.find_all { |turn| turn.correct? }
+  end
+
+  def number_turns_by_category(category)
+    @turns.find_all { |turn| turn.card.category == category }.length
+  end
+
+  def percent_correct
+    (number_correct.to_f / @turns.length) * 100
+  end
+  
+  def percent_correct_by_category(category)
+    (number_correct_by_category(category).to_f / number_turns_by_category(category)) * 100
+  end
+
+end
