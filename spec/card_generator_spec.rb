d9275f1c7c19fcc805dc031625cbea5f2d269d47
Kevin McGrevey <niv3kmcg@gmail.com>
2021-08-12 08:48:37 -0600

    Creates CardGenerator spec

---
+++ spec/card_generator_spec.rb
@@ -0,0 +1,19 @@
+require './lib/card_generator'
+
+describe CardGenerator do
+  it 'exists' do
+    expect(CardGenerator).to be_a Class
+  end
+
+  it 'creates a list of cards' do
+    filename = "cards.txt"
+    cards = CardGenerator.new(filename).cards
+
+    expect(cards.length).to eq(4)
+    expect(cards[0].question).to eq('What is the capital of Alaska?')
+    expect(cards[1].answer).to eq('Mars')
+    expect(cards[2].category).to eq('STEM')
+    expect(cards[3].keys.size).to eq(3)
+  end
+
+end
